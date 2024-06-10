1e38d4a68d52928e403ea2137fdf12f406198c7b
Jasur <rakhmatovjasur3@gmail.com>
2024-06-10 11:58:05 +0500

    Facemodel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import  { useEffect, useRef } from "react";
+import "./App.css";
+import * as tf from "@tensorflow/tfjs";
+import * as facemash from "@tensorflow-models/face-landmarks-detection";
+import Webcam from "react-webcam";
+import { drawMesh } from "./utils/util";
+const App = () => {
+  const webCamRef = useRef(null);
+  const canvasRef = useRef(null);
 
-function App() {
+  const runFacemash = async () => {
+
+    const net = await facemash.load(
+      facemash.SupportedPackages.mediapipeFacemesh
+    );
+    setInterval(() => {
+      detect(net);
+    }, 10);
+  };
+
+  const detect = async (net) => {
+    if (
+      typeof webCamRef.current !== "undefined" &&
+      webCamRef.current !== null &&
+      webCamRef.current.video.readyState === 4
+    ) {
+      //get video properties
+      const video = webCamRef.current.video;
+      const videoWidth = webCamRef.current.video.videoWidth;
+      const videoHeight = webCamRef.current.video.videoHeight;
+
+      //set video width
+      webCamRef.current.video.width = videoWidth;
+      webCamRef.current.video.height = videoHeight;
+
+      //set canvas width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      //detection
+      const face = await net.estimateFaces({ input: video });
+
+      // ctx get from canvas
+      const ctx = canvasRef.current.getContext('2d');
+      requestAnimationFrame(() => {
+        drawMesh(face,ctx)
+      });
+    }
+  };
+
+  useEffect(() => {
+    runFacemash();
+
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Webcam ref={webCamRef} className="webcam" />
+      
+      <canvas ref={canvasRef} className="canvas" />
       </header>
     </div>
   );
-}
+};
 
 export default App;
